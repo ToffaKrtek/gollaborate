d853d2a75c630e2f2340873b987082bc11754023
Krotov Anthon <krotov.anthon@yandex.ru>
2026-05-23 21:07:09 +1000

    room

--- main.go
+++ main.go
@@ -1,13 +1,68 @@
 package main
 
 import (
-	"github.com/gorilla/websocket"
 	"html/template"
 	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
 )
+
+var rooms = struct {
+	sync.RWMutex
+	m map[string][]*websocket.Conn
+}{m: make(map[string][]*websocket.Conn)}
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	doc := r.URL.Query().Get("doc")
+	if doc == "" {
+		http.Error(w, "missing doc parameter", http.StatusBadRequest)
+		return
+	}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		http.Error(w, "could no upgrade", http.StatusBadRequest)
+		return
+	}
+	rooms.Lock()
+	rooms.m[doc] = append(rooms.m[doc], conn)
+	rooms.Unlock()
+
+	defer func() {
+		rooms.Lock()
+		defer rooms.Unlock()
+		conns := rooms.m[doc]
+		for i, c := range conns {
+			if c == conn {
+				rooms.m[doc] = append(rooms.m[doc][:i], rooms.m[doc][i+1:]...)
+				break
+			}
+		}
+		if len(rooms.m[doc]) == 0 {
+			delete(rooms.m, doc)
+		}
+		conn.Close()
+	}()
+	for {
+		msgType, msg, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		rooms.RLock()
+		for _, c := range rooms.m[doc] {
+			if c == conn {
+				continue
+			}
+			if err := c.WriteMessage(msgType, msg); err != nil {
+				c.Close()
+			}
+		}
+		rooms.RUnlock()
+	}
 }
 
 func wsEchoHandler(w http.ResponseWriter, r *http.Request) {
@@ -45,5 +100,6 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/ws", wsEchoHandler)
+	http.HandleFunc("/ws", wsHandler)
 	http.ListenAndServe("0.0.0.0:8080", nil)
 }
--- main_test.go
+++ main_test.go
@@ -5,10 +5,41 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestWebSocketRooms(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		wsHandler(w, r)
+	}))
+	defer server.Close()
+	wsUrl := "ws" + strings.TrimPrefix(server.URL, "http")
+	conn1, _, err := websocket.DefaultDialer.Dial(wsUrl+"?doc=doc1", nil)
+	assert.NoError(t, err)
+	defer conn1.Close()
+	conn2, _, err := websocket.DefaultDialer.Dial(wsUrl+"?doc=doc1", nil)
+	assert.NoError(t, err)
+	defer conn2.Close()
+	conn3, _, err := websocket.DefaultDialer.Dial(wsUrl+"?doc=doc2", nil)
+	assert.NoError(t, err)
+	defer conn3.Close()
+
+	msg1 := "hello from client1"
+	err = conn1.WriteMessage(websocket.TextMessage, []byte(msg1))
+	assert.NoError(t, err)
+
+	_, msg2, err := conn2.ReadMessage()
+	assert.NoError(t, err)
+	assert.Equal(t, msg1, string(msg2))
+
+	conn3.SetReadDeadline(time.Now().Add(100 * time.Millisecond))
+	_, _, err = conn3.ReadMessage()
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "timeout")
+}
 
 func TestWebSocketEcho(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(wsEchoHandler))
